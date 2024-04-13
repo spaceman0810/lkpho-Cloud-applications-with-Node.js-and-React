3e2c4d784a296a7b70f6b77478068238b67361be
spaceman0810 <146539163+spaceman0810@users.noreply.github.com>
2024-04-13 18:05:05 +0100

    Lab Completed So Committing Clone
    Lab Completed So Committing Clone

--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,4 +1,4 @@
 module.exports.getDate = function getDate() {
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
+    let aestTime = new Date().toLocaleString("en-UK", {timeZone: "Australia/Brisbane"});
     return new Date(aestTime);
 }
